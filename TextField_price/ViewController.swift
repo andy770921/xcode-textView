edcb8d97778016f4e0b8b69d9fcf90a75e2e56a2
Andy Chou <Andy@andys-imac.local>
2016-09-25 00:48:07 +0800

    Initial Commit

---
+++ TextField_price/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextField_price
+//
+//  Created by Andy Chou on 2016/9/25.
+//  Copyright © 2016年 Andy Chou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
